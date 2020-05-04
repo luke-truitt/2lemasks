1f0dd91de62266543fd2c971672ff72ba588eaa2
LUKE TRUITT <lot@duke.edu>
2020-05-03 21:10:23 -0500

    update for netlify

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -51,7 +51,7 @@ class HomePage extends React.Component {
                 justifyContent: "center",
                 alignItems: "center",
                 width: "100%",
-                height: "100vh"
+                height: "100vh" 
               }}
             >
               {/**Icon */}
